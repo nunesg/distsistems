c9de175a12dbbe4380f057c3d58223107640b460
Gustavo <gustavonunes14.2011@gmail.com>
2021-08-16 20:17:04 -0300

    use proto messages on client/server communication

--- main_project/src/main/java/com/client/CustomerApp.java
+++ main_project/src/main/java/com/client/CustomerApp.java
@@ -3,15 +3,14 @@ package com.client;
 import java.net.*;
 import java.io.ObjectInputStream;
 
-public class ClientApp {
+import com.proto.CommonMessage;
+
+public class CustomerApp {
   public static void main(String[] args) {
 
     try {
       Socket server = new Socket("localhost", 12345);
-      ObjectInputStream stream = new ObjectInputStream(server.getInputStream());
-      String message = stream.readObject().toString();
-
-      System.out.println("Message received: " + message);
+      System.out.println("Message received: " + CommonMessage.parseFrom(server.getInputStream()).getContent());
       server.close();
     } catch (Exception e) {
       System.out.println("Exception on client. Message: " + e.toString());
