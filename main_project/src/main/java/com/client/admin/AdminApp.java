b47736b2b1fa77eb30218912a2a25c5416f49f77
Gustavo <gustavonunes14.2011@gmail.com>
2021-08-17 00:35:06 -0300

    setup gRPC for admin client and server

--- main_project/src/main/java/com/client/admin/AdminApp.java
+++ main_project/src/main/java/com/client/admin/AdminApp.java
@@ -1,22 +1,39 @@
 package com.client;
 
-import java.net.*;
-import java.io.ObjectInputStream;
+import io.grpc.Channel;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
 
-import com.proto.CommonMessage;
+import com.server.AdminPortalGrpc;
+import com.server.AdminPortalGrpc.AdminPortalBlockingStub;
+import com.server.HelloRequest;
+import com.server.HelloReply;
 
 public class AdminApp {
   private static final int PORT_NUMBER = 12340;
+  private static final String HOST_ADDRESS = "localhost";
+  private static final String TARGET = HOST_ADDRESS + ":" + PORT_NUMBER;
+
+  private final AdminPortalBlockingStub blockingStub;
+
+  public AdminApp(Channel channel) {
+    blockingStub = AdminPortalGrpc.newBlockingStub(channel);
+  }
+
+  public void sayHello() {
+    HelloRequest req = HelloRequest.newBuilder().setName("world").build();
+    HelloReply reply = blockingStub.sayHello(req);
+    System.out.println("Reply: " + reply.getMessage());
+  }
 
   public static void main(String[] args) {
+    ManagedChannel channel = ManagedChannelBuilder.forTarget(TARGET).usePlaintext().build();
+    AdminApp client = new AdminApp(channel);
 
     try {
-      Socket server = new Socket("localhost", PORT_NUMBER);
-      System.out.println("Message received: " + CommonMessage.parseFrom(server.getInputStream()).getContent());
-      server.close();
-    } catch (Exception e) {
-      System.out.println("Exception on client. Message: " + e.toString());
+      client.sayHello();
+    } finally {
+      channel.shutdown();
     }
-
   }
 }
