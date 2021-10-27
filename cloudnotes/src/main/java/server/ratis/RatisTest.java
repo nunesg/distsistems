d946439556aa0d6fa3abdaa0110b165da1adb89b
Gustavo <gustavonunes14.2011@gmail.com>
2021-10-27 16:52:23 -0300

    add RatisTest (base of communication between maven ratis and gradle project through sockets)

---
+++ cloudnotes/src/main/java/server/ratis/RatisTest.java
@@ -0,0 +1,27 @@
+package cloudnotes.server;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import java.net.*;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.BufferedReader;
+import java.util.Scanner;
+
+import cloudnotes.server.UsersCacheManager;
+
+public class RatisTest {
+  public static void main(String args[]) throws Exception {
+    Socket server = new Socket("localhost", 23456);
+    PrintWriter out = new PrintWriter(server.getOutputStream(), true);
+    BufferedReader in = new BufferedReader(new InputStreamReader(server.getInputStream()));
+
+    out.println(String.join(" ", args));
+    System.out.println(
+      "Status message received: " + in.readLine());
+    server.close();
+  }
+}
---
+++ cloudnotes/src/main/ratis/ChaveValor/src/main/java/Cliente.java
@@ -0,0 +1,116 @@
+import org.apache.ratis.client.RaftClient;
+import org.apache.ratis.conf.Parameters;
+import org.apache.ratis.conf.RaftProperties;
+import org.apache.ratis.grpc.GrpcFactory;
+import org.apache.ratis.protocol.*;
+import org.apache.ratis.thirdparty.com.google.protobuf.ByteString;
+
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.ServerSocket;
+import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Arrays;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
+
+public class Cliente
+{
+    public static void main(String[] args) throws IOException, ExecutionException, InterruptedException {
+        String raftGroupId = "raft_group____um"; // 16 caracteres.
+
+        Map<String,InetSocketAddress> id2addr = new HashMap<>();
+        id2addr.put("p1", new InetSocketAddress("127.0.0.1", 3000));
+        id2addr.put("p2", new InetSocketAddress("127.0.0.1", 3500));
+        id2addr.put("p3", new InetSocketAddress("127.0.0.1", 4000));
+
+        List<RaftPeer> addresses = id2addr.entrySet()
+                .stream()
+                .map(e -> RaftPeer.newBuilder().setId(e.getKey()).setAddress(e.getValue()).build())
+                .collect(Collectors.toList());
+
+        final RaftGroup raftGroup = RaftGroup.valueOf(RaftGroupId.valueOf(ByteString.copyFromUtf8(raftGroupId)), addresses);
+        RaftProperties raftProperties = new RaftProperties();
+
+        RaftClient client = RaftClient.newBuilder()
+                                      .setProperties(raftProperties)
+                                      .setRaftGroup(raftGroup)
+                                      .setClientRpc(new GrpcFactory(new Parameters())
+                                      .newRaftClientRpc(ClientId.randomId(), raftProperties))
+                                      .build();
+
+
+
+        int port = 23456;
+        try {
+            ServerSocket serverSocket = new ServerSocket(port);
+            System.out.println("Server listening on port " + port);
+        
+            try {
+        
+                while (true) {
+                    Socket clientSocket = serverSocket.accept();
+                    PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                    BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                    System.out.println("Client connected on port " + port + "!");
+                    
+                    String req = in.readLine();
+                    System.out.println("req: " + req);
+                    if (req != null) {
+                        out.println(serve(req.split(" "), client));
+                    }
+                    clientSocket.close();
+                }
+            } catch (Exception e) {
+                System.out.println("Server error. Message: " + e.getMessage());
+            }
+        
+            serverSocket.close();
+        } catch (Exception e) {
+            System.out.println("Error trying to setup server on port " + port);
+        }
+
+        client.close();
+    }
+    
+    public static String serve(String[] args, RaftClient client) throws Exception {
+        RaftClientReply getValue;
+        CompletableFuture<RaftClientReply> compGetValue;
+        String response;
+        System.out.println("args: " + Arrays.toString(args));
+        switch (args[0]){
+            case "add":
+                getValue = client.io().send(Message.valueOf("add:" + args[1] + ":" + args[2]));
+                response = getValue.getMessage().getContent().toString(Charset.defaultCharset());
+                System.out.println("Resposta: " + response);
+                return response;
+            case "get":
+                getValue = client.io().sendReadOnly(Message.valueOf("get:" + args[1]));
+                response = getValue.getMessage().getContent().toString(Charset.defaultCharset());
+                System.out.println("Resposta: " + response);
+                return response;
+            case "add_async":
+                compGetValue = client.async().send(Message.valueOf("add:" + args[1] + ":" + args[2]));
+                getValue = compGetValue.get();
+                response = getValue.getMessage().getContent().toString(Charset.defaultCharset());
+                System.out.println("Resposta: " + response);
+                return response;
+            case "get_stale":
+                getValue = client.io().sendStaleRead(Message.valueOf("get:" + args[1]), 0, RaftPeerId.valueOf(args[2]));
+                response = getValue.getMessage().getContent().toString(Charset.defaultCharset());
+                System.out.println("Resposta: " + response);
+                return response;
+            default:
+                System.out.println("Comando inválido");
+                return "Invalid command";
+        }
+    }
+
+}
